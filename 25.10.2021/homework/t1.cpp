eb52c4b61fa5ee7355b3533d8a3e44470ce6c731
mitrofanova_anna <a.mitrofanova@g.nsu.ru>
2021-11-05 13:48:12 +0300

    hw

--- 25.10.2021/homework/t1.cpp
+++ 25.10.2021/homework/t1.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
-    
+    int day, mon;
+    cin>>day>>mon;
+    int a = (14 - mon)/12;
+    int m = mon + 12*a - 2;
+    int y = 2021 - a;
+    while ((day + y + y/4 - y/100 + y/400 + (31*m)/12) % 7 != 5){
+        y++;
+    }
+    cout << y + a;
     return 0;
 }
