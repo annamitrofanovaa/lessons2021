bef8397d01013970fccc79e55dd1e6b74c58abdb
mitrofanova_anna <a.mitrofanova@g.nsu.ru>
2021-12-19 22:45:13 +0300

    11.10.2021

--- 11.10.2021/hw1.cpp
+++ 11.10.2021/hw1.cpp
@@ -13,13 +13,13 @@
 
 int main()
 {
-    int count = 0, denominator = 1;
+    int n = 0, denominator = 1;
     double acc, pi = 0;
 
     std::cin >> acc;
     do
     {
-        if (count % 2 == 0)
+        if (n % 2 == 0)
         {
             pi += double(4. / denominator);
         }
@@ -28,9 +28,25 @@ int main()
             pi -= double(4. / denominator);
         }
         denominator += 2;
-        ++count;
+        ++n;
     } while (std::fabs(pi - M_PI) >= acc);
-    std::cout << pi << ' ' << count << '\n';
+    std::cout << "pi_1 = " << pi << " n = " << n << '\n';
+
+    int R, count = 0;
+    std::cout<<"radius=";
+    std::cin >> R;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int x = rand() % R;
+        int y = rand() % R;
+        int point_radius = x * x + y * y;
+        if (point_radius <= R * R)
+        {
+            count++;
+        }
+    }
+    std::cout <<"pi_2 = "<<4.0 * (double)count / (double)n;
 
     return 0;
 }
